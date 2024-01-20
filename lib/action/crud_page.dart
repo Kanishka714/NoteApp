4f25a6dcaa26b7115b4313cb91b156b1e909db7f
Danusha Malshan <dan.malshan@gmail.com>
2024-01-21 01:22:16 +0530

    bug fixes

--- lib/action/crud_page.dart
+++ lib/action/crud_page.dart
@@ -5,20 +5,15 @@ import 'package:same/firebase_options.dart';
 import 'package:same/main.dart';
 
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-    runApp(const NoteApp());
-}
 
-class CrudPage extends StatelessWidget {
-  const CrudPage({super.key});
+// class CrudPage extends StatelessWidget {
+//   const CrudPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomePage(),
+//     );
+//   }
+// }
